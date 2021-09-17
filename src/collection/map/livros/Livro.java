f857dbe42301904fde24dd2fb633c8514f30f7ad
Raffes <rafaelaperes215@gmail.com>
2021-09-17 20:00:15 -0300

    Adicionado classe para ser usado no map

---
+++ src/collection/map/livros/Livro.java
@@ -0,0 +1,68 @@
+package collection.map.livros;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+
+public class Livro{
+    private String nome;
+    private Integer paginas;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livros = (Livro) o;
+        return nome.equals(livros.nome) && paginas.equals(livros.paginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, paginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+}
+
+class ComparatorNome implements Comparator<Map.Entry<String, Livro>> {
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return l1.getValue().getNome().compareToIgnoreCase(l2.getValue().getNome());
+    }
+}
+
+class ComparatorPaginas implements Comparator<Map.Entry<String, Livro>> {
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return Integer.compare(l1.getValue().getPaginas(), l2.getValue().getPaginas());
+    }
+}
